33f6577b0a581e2f2a435456c498d27d96fc8420
Xiah <xiah0526@gmail.com>
2022-06-28 09:55:44 +0900

    Springdoc와 Actuator을 dependency에 추가하고, 적용한다

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
