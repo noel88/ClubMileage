191750b95eaebd7a567310e8f240570a8162b603
Xiah <xiah0526@gmail.com>
2022-06-30 14:28:10 +0900

    불필요한 코드를 제거한다

--- src/main/kotlin/com/triple/clubmileage/repository/ReviewEventRepository.kt
+++ src/main/kotlin/com/triple/clubmileage/repository/ReviewEventRepository.kt
@@ -3,6 +3,4 @@ package com.triple.clubmileage.repository
 import com.triple.clubmileage.domain.ReviewEvent
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ReviewEventRepository: JpaRepository<ReviewEvent, String>, ReviewEventRepositorySupport {
-    fun countByPlaceId(placeId: String) : Int
-}
+interface ReviewEventRepository: JpaRepository<ReviewEvent, String>, ReviewEventRepositorySupport
